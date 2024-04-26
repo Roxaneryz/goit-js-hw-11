fbddbb484959bcd2df121a9de620555d1cfe5c23
Roxaneryz <roxane5666@yahoo.gr>
2024-04-26 19:56:12 +0300

    final

--- src/js/pixabay-api.js
+++ src/js/pixabay-api.js
@@ -6,18 +6,19 @@ const API_KEY = '42454875-eb3549610f297412779ff13b6';
  
 export function fetchImages (searchImg){
 
-  const url = `https://pixabay.com/api/?key=${API_KEY}&q=${searchImg}&image_type=photo&orientation=horizontal&safesearch=true`;
+  // const url = `https://pixabay.com/api/?key=${API_KEY}&q=${searchImg}&image_type=photo&orientation=horizontal&safesearch=true`;
 
-// const BASE_URL = 'https://pixabay.com/api';
-// const END_POINT = '/key';
-// const params = new URLSearchParams({
-//       q: 'searchImg',
-//       image_type:'photo',
-//       orientation: 'horizontal',
-//       safesearch: true
+const BASE_URL = 'https://pixabay.com/';
+const END_POINT = 'api/';
+const params = new URLSearchParams({
+      q: searchImg,
+      key: API_KEY,
+      image_type:'photo',
+      orientation: 'horizontal',
+      safesearch: true
    
-// })
-// const url = `${BASE_URL}${END_POINT}${API_KEY}${params}`;
+})
+const url = `${BASE_URL}${END_POINT}?${params}`;
 
 return fetch(url).then(response =>{
 
